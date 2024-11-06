faabb8232741029ed15edfc72d761ad658c72a6f
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-11-06 20:20:47 +0200

    FVCalv first try

--- FVCalc.java
+++ FVCalc.java
@@ -3,9 +3,8 @@ public class FVCalc {
 	public static void main(String[] args){
 		int currentValue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);
-		rate = rate / 100;
 		int n = Integer.parseInt(args[2]);
-		double FutureValue = currentValue * Math.pow(1 + rate, n);
+		double FutureValue = currentValue * Math.pow(1 + (rate/100), n);
 		System.out.println("After " + n + " years, $" + currentValue + " saved at " + rate + "% will yeild $" + (int) FutureValue);
 	}
 }
