ec20cf76717a37f2f526f8cdbcf9072e5ee4b656
nicole yerushalmi <nicole.yerushalmi@post.ac.il>
2024-11-06 19:10:19 +0200

    first try bill3

--- Bill3.java
+++ Bill3.java
@@ -6,6 +6,6 @@ public class Bill3 {
         String name3 = args[2];
         int sum = Integer.parseInt(args[3]);
         double pay = (sum + 0.0) / 3;
-        System.out.println("Dear " + name1 +", " + name2+ " and "+ name3 + ": pay " + Math.ceil(pay) + " Shekels each. ");
+        System.out.println("Dear " + name3 +", " + name2+ " and "+ name1 + ": pay " + Math.ceil(pay) + " Shekels each. ");
 	}
 }
